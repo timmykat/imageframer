a570af5efb0bdb0eea0e61efcd52df125a9ee4e0
Tim Kinnel <tkinnel@gmail.com>
2013-10-15 12:07:18 -0400

    Frameborder takes either percentage or pixels

--- jquery.imageframer.js
+++ jquery.imageframer.js
@@ -6,11 +6,13 @@
       frameWidth: 20,
       matteColor: '#555555',
       matteBorder: {
-        xpct: 0.10,
-        ypct: 0.10
+        x: '10%',
+        y: '10%'
       },
       innerColor: '#ffffff',
-      innerWidth: 3
+      innerWidth: 3,
+      imageHeight: 0,
+      imageWidth: 0
     }, options );
     
   
@@ -21,18 +23,24 @@
       }
       
       var $image = $(this);      
-      $image.load(function() {
-        var thisImage = 'image-' + $('.frame-matte').length;
-        $image.wrap('<div id="' + thisImage + '" class="frame-matte"></div>');
-        var w = $image.width();
-        var h = $image.height();
+      $image.load(function(e) {
+        $image.wrap('<div class="frame-matte"></div>');
+        var w = Math.max($image.width(), settings.imageWidth);
+        var h = Math.max($image.height(), settings.imageHeight);
         var matte = new Object();
-        matte.leftBorder = w * settings.matteBorder.xpct
-        matte.topBorder = h * settings.matteBorder.ypct
+        if (settings.matteBorder.x.match(/%/)) {
+          matte.leftBorder = w * parseFloat(settings.matteBorder.x.replace('%', ''))/100.0
+        } else {
+          matte.leftBorder = parseInt(settings.matteBorder.x.replace('px', ''))
+        }
+        if (settings.matteBorder.y.match(/%/)) {
+          matte.topBorder = h * parseFloat(settings.matteBorder.y.replace('%', ''))/100.0
+        } else {
+          matte.topBorder = parseInt(settings.matteBorder.y.replace('px', ''))
+        }
         matte.w = w + (2 * matte.leftBorder)
         matte.h = h + (2 * matte.topBorder)
-        console.log(matte)
-        $('#' + thisImage).css({ 'width': (matte.w + 2*settings.frameWidth) + 'px', 'height': (matte.h + 2*settings.frameWidth) + 'px', 'position': 'relative', 'background-color': settings.matteColor, 'border-width': settings.frameWidth + 'px', 'border-color': settings.frameColor, 'border-style' : 'solid'  });
+        $image.parent().css({ 'width': (matte.w + 2*settings.frameWidth) + 'px', 'height': (matte.h + 2*settings.frameWidth) + 'px', 'position': 'relative', 'background-color': settings.matteColor, 'border-width': settings.frameWidth + 'px', 'border-color': settings.frameColor, 'border-style' : 'solid'  });
         $image.css({ 'position': 'absolute', 'left': (matte.leftBorder + settings.frameWidth - settings.innerWidth) + 'px', 'top': (matte.topBorder + settings.frameWidth - settings.innerWidth) + 'px', 'border-width': settings.innerWidth + 'px', 'border-color': settings.innerColor, 'border-style' : 'solid'    });
       });       
     });
